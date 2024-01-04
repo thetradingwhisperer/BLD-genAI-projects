8aebdad2378fca06c98e137ec042a770130a9edb
The Trading Whisperer <29692338+thetradingwhisperer@users.noreply.github.com>
2024-01-04 13:05:57 +0000

    Added env variable

--- chatGPT_clone/app.py
+++ chatGPT_clone/app.py
@@ -1,12 +1,17 @@
-from flask import Flask 
+from flask import Flask, render_template
+import os
 
 # Create the application object
 app = Flask(__name__)
 
+#API key
+api_key = os.getenv('API_KEY')
+print(api_key)
+
 #Home route
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
 
 
 # Start the server with the 'run()' method
