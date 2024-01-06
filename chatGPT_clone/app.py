40b7dbdb3fd9c8a0bab9723a035e7df1caf82296
The Trading Whisperer <29692338+thetradingwhisperer@users.noreply.github.com>
2024-01-06 14:33:35 +0000

    Aded html templates

--- chatGPT_clone/app.py
+++ chatGPT_clone/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-import os
+from flask import Flask, request, render_template
+import os, json, openai
 
 # Create the application object
 app = Flask(__name__)
@@ -12,6 +12,22 @@ print(api_key)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+#Get bot response
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=userText,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.9,
+    )
+
+    answer = response["choices"][0]["text"]
+    return str(answer)
 
 
 # Start the server with the 'run()' method
